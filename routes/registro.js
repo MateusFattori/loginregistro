fd91d4d6e4ef4c6c748fe078947fa208324cd0d5
MateusFattori <83764798+MateusFattori@users.noreply.github.com>
2021-10-07 08:48:49 -0300

    Add files via upload

--- routes/registro.js
+++ routes/registro.js
@@ -1,4 +1,7 @@
 module.exports = (app)=>{
+    //Importar o bcrypt
+    var bcrypt = require('bcrypt')
+
     app.get('/registro',(req,res)=>{
         res.render('registro.ejs')
     })
@@ -11,10 +14,11 @@ module.exports = (app)=>{
         if(usereexiste){
             return res.send('Email já cadastrado')
         }else{
+            var senhasegura = await bcrypt.hash(req.body.senha,12)
             var documento = new usuarios({
                 nome:req.body.nome,
                 email:req.body.email,
-                senha:req.body.senha
+                senha:senhasegura
             }).save()
             res.render('login.ejs')
         }
