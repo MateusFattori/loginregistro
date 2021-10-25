d899db976aa32654cdd0445196744e970bddbfc4
MateusFattori <83764798+MateusFattori@users.noreply.github.com>
2021-10-25 11:42:46 -0300

    Add files via upload

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,4 +1,8 @@
 module.exports = (app)=>{
+    var conexao = require("../config/database")
+    var usuarios = require("../models/usuarios")
+    var atividades = require("../models/atividades")
+    
     app.get('/dashboard',async(req,res)=>{
         var id = req.query.id
         console.log(id)
@@ -13,5 +17,21 @@ module.exports = (app)=>{
             }
         }   
         res.render('dashboard.ejs',{login:{nome:'teste', _id:'teste'}})
+    })
+
+    app.post('/dashboard',async(req,res)=>{
+        var dados = req.body
+
+        var novo = await new atividades({
+            data: dados.data,
+            tipo: dados.tipo,
+            entrega: dados.entrega,
+            disiclina: dados.disiclina,
+            instrucoes: dados.instrucoes,
+            user: dados.id
+        }).save()
+        .then((result)=>{
+            res.redirect('/dashboard?id='+dados.id)
+        })
     })
 }
